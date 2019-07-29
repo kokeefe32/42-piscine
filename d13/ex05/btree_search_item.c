86767a897c0e782eb7cdeb7abb25a067da2095cd
Kayli O'keefe <kaokeefe@e1z2r1p12.42.us.org>
2019-07-29 09:58:59 -0700

    fix typo

--- d13/ex05/btree_search_item.c
+++ d13/ex05/btree_search_item.c
@@ -6,7 +6,7 @@
 /*   By: kaokeefe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/26 11:19:11 by kaokeefe          #+#    #+#             */
-/*   Updated: 2019/07/26 11:43:24 by kaokeefe         ###   ########.fr       */
+/*   Updated: 2019/07/29 09:58:50 by kaokeefe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,6 @@ void	*btree_search_item(t_btree *root, void *data_ref,
 	if (!res && cmpf(root->item, data_ref) == 0)
 		res = root->item;
 	if (!res && root->right)
-		res = btree_search_item(root->left, data_ref, cmpf);
+		res = btree_search_item(root->right, data_ref, cmpf);
 	return (res);
 }
